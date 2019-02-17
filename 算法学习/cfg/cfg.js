c8ccf334520c1e0e9fed1a09dd4927716a704fda
qoxop <1106291186@qq.com>
2019-02-17 12:40:18 +0800

    test github page

---
+++ 算法学习/cfg/cfg.js
@@ -0,0 +1,11 @@
+const accessKey = 'lrvL9b5DF16U91bBeBQjDbbbKeT2jn3HW3gmu0TX';
+const secretKey = 'GK6wvXldqHqN7tYdAsametNTvM-xgCmyHNM5DDWA';
+const Zone = {
+    HUANAN: 'qiniu.zone.Zone_z2',
+    HUADONG: 'qiniu.zone.Zone_z0',
+    HUABEI: 'qiniu.zone.Zone_z1',
+    BEIMEI:'qiniu.zone.Zone_na0',
+}
+exports.accessKey = accessKey;
+exports.secretKey = secretKey;
+exports.Zone = Zone;
